ca81a3a6003814c3d4136fd1252b40a3d0d22c47
Abhiiyer02 <93037572+Abhiiyer02@users.noreply.github.com>
2022-10-28 00:59:21 +0530

    Delete variation1.py

--- variation1.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Enter the number of frames: "))
-frames=[]
-print("Enter frame number and data(for): ")
-for i in range(n):
-    nums,data=input().split(" ")
-    nums=int(nums)
-    frames.append([nums,data])
-frames.sort()
-print(frames)
