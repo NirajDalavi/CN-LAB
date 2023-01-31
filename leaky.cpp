c455e3ba27a1517e4e7b9211178ed570a5292ee3
Sriraam-K <116096802+Sriraam-K@users.noreply.github.com>
2023-01-31 20:50:47 +0530

    Random IP rate

--- leaky.cpp
+++ leaky.cpp
@@ -4,27 +4,32 @@ using namespace std;
 
 int main()
 {
-    int bucket_size,ip_rate,op_rate,time_steps,cur_size=0;
+    int bucket_size,op_rate,time_steps,cur_size=0;
     cout<<"Enter the bucket size: ";
     cin>>bucket_size;
-    cout<<"Enter input rate:";
-    cin>>ip_rate;
-    cout<<"Enter the output rate:";
-    cin>>op_rate;
     cout<<"Enter the steps to simulate:";
     cin>>time_steps;
+    cout<<"Enter the variable input rates for "<<time_steps<<" seconds: ";
+    int ip_rate[time_steps];
+    for(int i=0;i<time_steps;i++)
+        cin>>ip_rate[i];
+    cout<<"Enter the output rate:";
+    cin>>op_rate;
     for(int i=0;i<time_steps;i++)
     {
         int space_left=bucket_size-cur_size;
         int loss=0;
-        if(ip_rate<=space_left)
-            cur_size+=ip_rate;
+        if(ip_rate[i]<=space_left)
+            cur_size+=ip_rate[i];
         else
         {
-            loss=ip_rate-space_left;
+            loss=ip_rate[i]-space_left;
             cur_size=bucket_size;
         }
-        cur_size-=op_rate;
+        if(cur_size>op_rate)
+            cur_size-=op_rate;
+        else
+            cur_size=0;    
         cout<<"Time="<<i<<"s | Bucket/Buffer status:"<<cur_size<<"/"<<bucket_size<<", Packet loss= "<<loss<<endl;
     }
 }
