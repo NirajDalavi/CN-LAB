cc2e11ceacbe096d27f244560e9434095c831963
Abhiiyer02 <93037572+Abhiiyer02@users.noreply.github.com>
2023-02-01 23:44:18 +0530

    Update udpclient.java

--- udpclient.java
+++ udpclient.java
@@ -8,14 +8,15 @@ public class udpclient{
                 aSocket=new DatagramSocket(clientPort);
                 byte[] buf=new byte[1000];
                 byte[] buf1=new byte[1000];
+                byte[] msg=new byte[1000];
+            
                 DatagramPacket data=new DatagramPacket(buf,buf.length);
                 String conf="connected to client";
                 buf1=conf.getBytes();
                 DatagramPacket data1=new DatagramPacket(buf1,buf1.length,InetAddress.getLocalHost(),999);
                 aSocket.send(data1);
                 System.out.println("connected to server");
-                aSocket.receive(data);
-                byte[] msg=new byte[1000];
+                aSocket.receive(data);                
                 msg=data.getData();
                 System.out.println("\n message:");
                 System.out.println(new String(msg,0,data.getLength()));
